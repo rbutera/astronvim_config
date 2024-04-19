9bb599d51c88a1427e5c1edd26b744934b76e224
Rai Butera <rai@rbutera.com>
2024-04-19 17:40:58 +0100

    migrate from v3

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -23,24 +23,22 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
         " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+        " ",
+        " ",
+        " ",
+        " ██████  ██      ██   ██ ███████",
+        "██       ██      ██   ██ ██    ",
+        "██   ███ ██      ███████ █████ ",
+        "██    ██ ██      ██   ██ ██    ",
+        " ██████  ███████ ██   ██ ██    ",
+        " ",
+        " ",
+        " ",
       }
       return opts
     end,
   },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
@@ -53,33 +51,119 @@ return {
     end,
   },
 
+  { "j-hui/fidget.nvim", lazy = false },
+  { "VonHeikemen/searchbox.nvim", lazy = false },
   {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
+    "nvim-lua/plenary.nvim",
+    lazy = false,
+  },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    requires = "nvim-lua/plenary.nvim",
+    config = function() require("todo-comments").setup {} end,
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function() require("trouble").setup {} end,
+  },
+  {
+    "folke/lsp-colors.nvim",
+    lazy = false,
+  },
+  {
+    "kosayoda/nvim-lightbulb",
+    lazy = false,
+    requires = "antoinemadec/FixCursorHold.nvim",
+    config = function() require("nvim-lightbulb").setup { autocmd = { enabled = true } } end,
+  },
+  {
+    "bkad/CamelCaseMotion",
+    lazy = false,
+    config = function() vim.api.nvim_set_var("camelcasemotion_key", "<leader>") end,
+  },
+  {
+    "tpope/vim-eunuch",
+    lazy = false,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    config = function()
+      -- vim.o.foldcolumn = "0"
+      -- vim.o.foldnestmax = 3
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+      local ftMap = {
+        vim = "indent",
+        python = { "indent" },
+        git = "",
+      }
+
+      local function customizeSelector(bufnr)
+        local function handleFallbackException(err, providerName)
+          if type(err) == "string" and err:match "UfoFallbackException" then
+            return require("ufo").getFolds(providerName, bufnr)
+          else
+            return require("promise").reject(err)
+          end
+        end
+
+        return require("ufo")
+          .getFolds("lsp", bufnr)
+          :catch(function(err) return handleFallbackException(err, "treesitter") end)
+          :catch(function(err) return handleFallbackException(err, "indent") end)
+      end
+
+      require("ufo").setup {
+        provider_selector = function(bufnr, filetype, buftype) return ftMap[filetype] or customizeSelector end,
+      }
     end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function() require("Comment").setup() end,
+  },
+  {
+    "nat-418/boole.nvim",
+    lazy = false,
+    command = "Boole",
+    config = function()
+      require("boole").setup {
+        mappings = {
+          increment = "<C-a>",
+          decrement = "<C-x>",
+        },
+      }
+    end,
+  },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+    },
   },
 }
