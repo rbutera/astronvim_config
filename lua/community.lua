8dd5939abb25ebcaa93f5ac7daef10c82ff2abff
Rai Butera <rai@rbutera.com>
2024-08-10 19:30:59 +0100

    swap copilot for supermaven

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- example of importing an entire language pack
   -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
   { import = "astrocommunity.pack.rust" },
