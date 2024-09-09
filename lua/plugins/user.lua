ca0c096a45763e3169941bc0012e2c98f0b2520d
Rai Butera <rai@rbutera.com>
2024-09-09 17:40:05 +0100

    add new multi cursor plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -154,9 +154,35 @@ return {
       }
     end,
   },
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- },
   {
-    "mg979/vim-visual-multi",
-    lazy = false,
+    "brenton-leighton/multiple-cursors.nvim",
+
+    opts = {}, -- This causes the plugin setup function to be called
+    keys = {
+      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
+      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+
+      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
+      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+
+      { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or remove cursor" },
+      -- {"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
+      -- {"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
+
+      {
+        "<Leader>a",
+        "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+        mode = { "n", "x" },
+        desc = "Add cursor and jump to next cword",
+      },
+      { "<Leader>A", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Skip to next cword" },
+      -- { "<Leader>A", "<Cmd>MultipleCursorsAddJumpNextMatchV<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+      -- {"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
+    },
   },
   {
     "numToStr/Comment.nvim",
